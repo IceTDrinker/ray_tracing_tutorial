18b4aee8c4e1daa465cd3952c3bd570da4e47d58
IceTDrinker <49040125+IceTDrinker@users.noreply.github.com>
2020-05-09 14:38:38 +0200

    Add stb submodule
    Modify code to save to png to save some space

--- src/color.h
+++ src/color.h
@@ -3,6 +3,7 @@
 #include "vec3.h"
 
 #include <iostream>
+#include <vector>
 
 void write_color(std::ostream& out, color pixel_color)
 {
@@ -10,4 +11,11 @@ void write_color(std::ostream& out, color pixel_color)
     out << static_cast<int>(255.999 * pixel_color.x()) << ' '
         << static_cast<int>(255.999 * pixel_color.y()) << ' '
         << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+}
+
+void write_color(unsigned char pixel_data[3], color pixel_color)
+{
+    pixel_data[0] = static_cast<unsigned char>(255.999 * pixel_color.x());
+    pixel_data[1] = static_cast<unsigned char>(255.999 * pixel_color.y());
+    pixel_data[2] = static_cast<unsigned char>(255.999 * pixel_color.z());
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,23 @@
 #include <cstdlib>
 #include <ctime>
 
+#include <filesystem>
 #include <fstream>
 #include <iostream>
-
-#include <filesystem>
 #include <stdexcept>
+#include <vector>
 
 namespace fs = std::filesystem;
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
 #pragma warning(pop)
 
 #include "color.h"
 #include "ray.h"
 
-static constexpr const char* output_dir = "outputs";
+static constexpr const char* output_dir = "outputs/";
 
 // Source : https://stackoverflow.com/questions/997946/how-to-get-current-time-and-date-in-c
 // Get current date/time, format is YYYY-MM-DD_HH-mm-ss
@@ -34,33 +37,45 @@ const std::string currentDateTime()
     return buf;
 }
 
-bool hit_sphere(const point3& center, double radius, const ray& r)
+double hit_sphere(const point3& center, double radius, const ray& r)
 {
     vec3 oc = r.origin() - center;
     auto a = dot(r.direction(), r.direction());
     auto b = 2.0 * dot(oc, r.direction());
     auto c = dot(oc, oc) - radius * radius;
     auto discriminant = b * b - 4 * a * c;
-    return (discriminant > 0);
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
 color ray_color(const ray& r)
 {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0)
     {
-        return color(1, 0, 0);
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
+    t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 int main(int /*argc*/, char* /*argv[]*/)
 {
     static constexpr const auto aspect_ratio = 16.0 / 9.0;
-    static constexpr const int image_width = 384;
+    static constexpr const int image_width = 1920;
     static constexpr const int image_height = static_cast<int>(image_width / aspect_ratio);
+    static constexpr const int num_channels = 3;
+
+    auto image = std::vector<unsigned char>(image_width * image_height * num_channels);
 
     auto output_dir_path = fs::path(output_dir);
 
@@ -74,12 +89,7 @@ int main(int /*argc*/, char* /*argv[]*/)
         }
     }
 
-    auto out_filename = currentDateTime() + ".ppm";
-    auto out_file_path = output_dir_path.append(out_filename);
-    auto output_file = std::ofstream(out_file_path);
-    auto& outstream = output_file;
-
-    outstream << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    auto out_filename = output_dir + currentDateTime() + ".png";
 
     point3 origin(0.0, 0.0, 0.0);
     vec3 horizontal(4.0, 0.0, 0.0);
@@ -92,13 +102,15 @@ int main(int /*argc*/, char* /*argv[]*/)
         for (int i = 0; i < image_width; ++i)
         {
             auto u = double(i) / (image_width - 1);
-            auto v = double(j) / (image_height - 1);
+            auto v = double(image_height - 1 - j) / (image_height - 1); // Image data is inverted on the y axis because of chosen coordinate system
             ray r(origin, lower_left_corner + u * horizontal + v * vertical);
             color pixel_color = ray_color(r);
-            write_color(outstream, pixel_color);
+            auto pixel_index = num_channels * (j * image_width + i);
+            write_color(&image.data()[pixel_index], pixel_color);
         }
-        outstream << "\n";
     }
+
+    stbi_write_png(out_filename.c_str(), image_width, image_height, 3, image.data(), image_width * num_channels * sizeof(unsigned char));
 
     std::cerr << "\nDone.\n";
 
