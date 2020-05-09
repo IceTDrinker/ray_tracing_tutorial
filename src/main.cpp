89a221a222b0dc0fd64356ce4f01ac21b1101029
IceTDrinker <49040125+IceTDrinker@users.noreply.github.com>
2020-05-09 13:51:55 +0200

    Code for first ray traced image with red sphere

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@ namespace fs = std::filesystem;
 #pragma warning(pop)
 
 #include "color.h"
+#include "ray.h"
 
 static constexpr const char* output_dir = "outputs";
 
@@ -33,10 +34,33 @@ const std::string currentDateTime()
     return buf;
 }
 
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
+color ray_color(const ray& r)
+{
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+    {
+        return color(1, 0, 0);
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
 int main(int /*argc*/, char* /*argv[]*/)
 {
-    static constexpr const int image_width = 256;
-    static constexpr const int image_height = 256;
+    static constexpr const auto aspect_ratio = 16.0 / 9.0;
+    static constexpr const int image_width = 384;
+    static constexpr const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     auto output_dir_path = fs::path(output_dir);
 
@@ -51,21 +75,26 @@ int main(int /*argc*/, char* /*argv[]*/)
     }
 
     auto out_filename = currentDateTime() + ".ppm";
-
     auto out_file_path = output_dir_path.append(out_filename);
-
     auto output_file = std::ofstream(out_file_path);
-
     auto& outstream = output_file;
 
     outstream << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    point3 origin(0.0, 0.0, 0.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, horizontal.x() / aspect_ratio, 0.0);
+    point3 lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, 1);
 
     for (int j = image_height - 1; j >= 0; --j)
     {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i)
         {
-            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+            color pixel_color = ray_color(r);
             write_color(outstream, pixel_color);
         }
         outstream << "\n";
