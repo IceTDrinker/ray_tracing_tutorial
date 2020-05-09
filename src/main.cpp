ebb8aed2a935f5313ba4ee96dced8075098bdef3
IceTDrinker <49040125+IceTDrinker@users.noreply.github.com>
2020-05-09 16:47:15 +0200

    After reading some github issues it appears threading is easy, and indeed it is
    8 threads for my 8 hyper threads CPU, no progress report because it would
    not make much sense in the console and require some synchronization to get
    right

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <memory>
 #include <stdexcept>
+#include <thread>
 #include <vector>
 
 namespace fs = std::filesystem;
@@ -64,6 +65,7 @@ int main(int /*argc*/, char* /*argv[]*/)
     static constexpr const int image_height = static_cast<int>(image_width / aspect_ratio);
     static constexpr const int num_channels = 3;
     static constexpr const int samples_per_pixel = 100;
+    static constexpr const int num_threads = 8;
 
     auto image = std::vector<unsigned char>(image_width * image_height * num_channels);
 
@@ -86,27 +88,55 @@ int main(int /*argc*/, char* /*argv[]*/)
     world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100));
     camera cam{aspect_ratio};
 
-    for (int j = image_height - 1; j >= 0; --j)
+    auto process_rows = [&](int start_j, int stop_j)
     {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i)
+        for (int j = stop_j - 1; j >= start_j; --j)
         {
-            color pixel_color(0, 0, 0);
-            for (int s = 0; s < samples_per_pixel; ++s)
+            //std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            for (int i = 0; i < image_width; ++i)
             {
-                auto u = (i + random_double()) / (image_width - 1);
-                auto v = (image_height - 1 - j + random_double()) / (image_height - 1);
-                ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                color pixel_color(0, 0, 0);
+                for (int s = 0; s < samples_per_pixel; ++s)
+                {
+                    auto u = (i + random_double()) / (image_width - 1);
+                    auto v = (image_height - 1 - j + random_double()) / (image_height - 1);
+                    ray r = cam.get_ray(u, v);
+                    pixel_color += ray_color(r, world);
+                }
+                auto pixel_index = num_channels * (j * image_width + i);
+                write_color(&image.data()[pixel_index], pixel_color, samples_per_pixel);
             }
-            auto pixel_index = num_channels * (j * image_width + i);
-            write_color(&image.data()[pixel_index], pixel_color, samples_per_pixel);
         }
+    };
+
+    std::vector<std::thread> threads;
+    threads.reserve(num_threads);
+
+    auto work_size = image_height / num_threads;
+    auto remaining_work = image_height % num_threads;
+
+    for (auto thread_idx = 0; thread_idx < num_threads; ++thread_idx)
+    {
+        auto start_row = thread_idx * work_size;
+        auto stop_row = start_row + work_size;
+        if (thread_idx < num_threads - 1)
+        {
+            threads.push_back(std::thread(process_rows, start_row, stop_row));
+        }
+        else
+        {
+            threads.push_back(std::thread(process_rows, start_row, stop_row + remaining_work));
+        }
+    }
+
+    for (auto& thread : threads)
+    {
+        thread.join();
     }
 
     stbi_write_png(out_filename.c_str(), image_width, image_height, 3, image.data(), image_width * num_channels * sizeof(unsigned char));
 
-    std::cerr << "\nDone.\n";
+    std::cerr << "Done.\n";
 
     return EXIT_SUCCESS;
 }
